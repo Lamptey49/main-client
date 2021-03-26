ddd538fdd851b31f96f5bcd1745cd1e7df620b2a
Lamptey Phinehas <lampteyphinehas70@gmail.com>
2021-03-26 00:37:23 +0000

    modify coreui

--- server/express.js
+++ server/express.js
@@ -23,7 +23,7 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 
 import MainRouter from './../client/MainRouter'
-import {  StaticRouter , Switch} from 'react-router-dom'
+import {  StaticRouter } from 'react-router-dom'
 import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
 import theme from './../client/theme'
 
